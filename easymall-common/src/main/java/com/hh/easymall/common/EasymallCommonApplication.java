df3fab2233e1eb1c1a92963c9623fef1adb76319
uu-hh <364721203@qq.com>
2020-11-24 23:19:09 +0800

    common服务

---
+++ easymall-common/src/main/java/com/hh/easymall/common/EasymallCommonApplication.java
@@ -0,0 +1,13 @@
+package com.hh.easymall.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymallCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymallCommonApplication.class, args);
+	}
+
+}
---
+++ easymall-common/src/test/java/com/hh/easymall/common/EasymallCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh.easymall.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymallCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
